af04d346dc428e5a94943d80d820b6adaa301cd8
Vadik <vadimushka4@gmail.com>
2018-12-06 21:14:22 +0200

    repeated objects & arraylist

---
+++ src/Car.java
@@ -0,0 +1,38 @@
+public class Car {
+
+
+    private int speed;
+    private String colour;
+    private int price;
+
+    public Car(int speed, String colour, int price) {
+        this.speed = speed;
+        this.price = price;
+        this.colour = colour;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,34 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Huj");
-        // Pizda - huj
-        // ya eby losey
-        // ebat evreev
-        k
+        Car toyota = new Car(130, "red", 2400);
+
+        System.out.println(toyota.getSpeed() + toyota.getColour() + toyota.getPrice());
+
+        toyota.setSpeed(190);
+        toyota.setColour("Black");
+        toyota.setPrice(3600);
+
+        System.out.println(toyota.getSpeed() + toyota.getColour() + toyota.getPrice());
+
+        Car[] cars = new Car[3];
+
+        cars[0] = toyota;
+        cars[1] = new Car(220, "white", 4000);
+        cars[2] = new Car(230, "brown", 4200);
+
+        for (int i = 0; i < cars.length; i++){
+            System.out.println(cars[i].getSpeed() + " " + cars[i].getColour() + " " + cars[i].getPrice());
+        }
+
+        ArrayList<Car> carsList = new ArrayList<>();
+        carsList.add(toyota);
+        carsList.add(new Car(220, "white", 4000));
+        carsList.add(new Car(230, "brown", 4200));
+
+        for (int i = 0; i < carsList.size(); i++){
+            System.out.println(carsList.get(i).getSpeed() + carsList.get(i).getColour() + carsList.get(i).getPrice());
+        }
     }
 }
